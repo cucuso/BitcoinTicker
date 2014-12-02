07c3940ac921970bcf880ca91d4eeeb170694068
edenn001 <edenn001@fiu.edu>
2014-12-01 23:55:59 -0500

    added radial background and the method to work onResume rather than create

--- app/src/main/java/com/eduardodennis/bitcointicker/ui/MainActivity.java
+++ app/src/main/java/com/eduardodennis/bitcointicker/ui/MainActivity.java
@@ -3,11 +3,20 @@ package com.eduardodennis.bitcointicker.ui;
 
 import com.eduardodennis.bitcointicker.R;
 import com.eduardodennis.bitcointicker.service.BitcoinTickerGenerator;
+import com.eduardodennis.bitcointicker.service.Graph;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.GraphViewSeries;
+import com.jjoe64.graphview.LineGraphView;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
+    int i = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +29,9 @@ public class MainActivity extends Activity {
 
 
 
-  /*
+
+
+
 
         //Graph init
         new Graph(this).execute();
@@ -29,11 +40,11 @@ public class MainActivity extends Activity {
 
 
         // init example series data
-        GraphViewSeries exampleSeries = new GraphViewSeries(new GraphViewData[] {
-                new GraphViewData(1, 2.0d)
-                , new GraphViewData(2, 1.5d)
-                , new GraphViewData(3, 2.5d)
-                , new GraphViewData(4, 1.0d)
+        GraphViewSeries exampleSeries = new GraphViewSeries(new GraphView.GraphViewData[] {
+                new GraphView.GraphViewData(1, 2.0d)
+                , new GraphView.GraphViewData(2, 1.5d)
+                , new GraphView.GraphViewData(3, 2.5d)
+                , new GraphView.GraphViewData(4, 1.0d)
         });
 
         GraphView graphView = new LineGraphView(
@@ -51,7 +62,17 @@ public class MainActivity extends Activity {
          View contentView = findViewById(R.id.graphLayout);
 
 
-        */
+
+
+
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        new BitcoinTickerGenerator(MainActivity.this).execute();
 
 
     }
